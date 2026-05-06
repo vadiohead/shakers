d593fc10078a2a0e90f846e153962079dbb5190a
vadiohead <ehutyuh@gmail.com>
2026-05-05 17:53:08 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // stop the refresh
+
+    const btn = document.getElementById("sendMessageButton");
+    btn.innerText = "Sending...";
+    btn.disabled = true;
+
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("service_dshc2fd", "template_wai7aeg", params)
+        .then(function(res) {
+            alert("Your message has been sent successfully!");
+            document.getElementById('contactForm').reset();
+            btn.innerText = "Send Message";
+            btn.disabled = false;
+        },
+        function(error) {
+            alert("Failed to send message. Error: " + JSON.stringify(error));
+            btn.innerText = "Send Message";
+            btn.disabled = false;
+        });
+});
